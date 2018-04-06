a6d20d918889b51882fa5d56cd6afdcd5b2d78b4
Oscar Dub <oscardub@gmail.com>
2018-04-06 19:27:00 +0100

    Delete chorales without notes.

--- server/src/seeders/20180403141204-music21-chorales.js
+++ server/src/seeders/20180403141204-music21-chorales.js
@@ -1,47 +1,61 @@
 const fs = require('fs');
+const sequelize = require('sequelize');
 
 const models = require('../models');
 
-const seedChorales = (queryInterface, next) => {
-  const chorales = fs.createReadStream('./data/chorales.txt');
-  let overhang = '';
+const SQL_DELETE_NOTELESS_CHORALES = `
+DELETE FROM "Chorales"
+WHERE bwv
+IN (
+  SELECT bwv FROM "Chorales"
+  EXCEPT
+  SELECT DISTINCT source FROM "Notes"
+);
+`;
 
-  chorales.on('data', chunk => {
-    const lines = `${overhang}${chunk.toString()}`.split('\n');
-    overhang = lines.slice(-1)[0];
-    const chorales = lines.slice(0, -1);
-    models.Chorale.bulkCreate(chorales.map(JSON.parse));
+const seedChorales = queryInterface =>
+  new Promise((resolve, reject) => {
+    const chorales = fs.createReadStream('./data/chorales.txt');
+    let overhang = '';
+
+    chorales.on('data', chunk => {
+      const lines = `${overhang}${chunk.toString()}`.split('\n');
+      overhang = lines.slice(-1)[0];
+      const chorales = lines.slice(0, -1);
+      models.Chorale.bulkCreate(chorales.map(JSON.parse));
+    });
+
+    chorales.on('end', () => {
+      resolve();
+    });
   });
 
-  chorales.on('end', () => {
-    next(null);
-  });
-};
+const seedNotes = queryInterface =>
+  new Promise((resolve, reject) => {
+    const notes = fs.createReadStream('./data/notes.txt');
+    let overhang = '';
 
-const seedNotes = (queryInterface, next) => {
-  const notes = fs.createReadStream('./data/notes.txt');
-  let overhang = '';
+    notes.on('data', chunk => {
+      const lines = `${overhang}${chunk.toString()}`.split('\n');
+      overhang = lines.slice(-1)[0];
+      const notes = lines.slice(0, -1);
+      models.Note.bulkCreate(notes.map(JSON.parse));
+    });
 
-  notes.on('data', chunk => {
-    const lines = `${overhang}${chunk.toString()}`.split('\n');
-    overhang = lines.slice(-1)[0];
-    const notes = lines.slice(0, -1);
-    models.Note.bulkCreate(notes.map(JSON.parse));
+    notes.on('end', () => {
+      resolve();
+    });
   });
-
-  notes.on('end', () => {
-    next(null);
-  });
-};
 
 module.exports = {
   up: (queryInterface, Sequelize, next) => {
-    seedChorales(queryInterface, (error, chorales) => {
-      seedNotes(queryInterface, () => {
-        console.log('done');
-        next(null);
-      });
-    });
+    seedChorales(queryInterface)
+      .then(() => seedNotes(queryInterface))
+      // Sync models to initialize indexes
+      .then(() => models.Note.sync())
+      // Delete Chorales with no associated notes
+      .then(() => queryInterface.sequelize.query(SQL_DELETE_NOTELESS_CHORALES))
+      .then(() => next());
   },
 
   down: (queryInterface, Sequelize) => {
