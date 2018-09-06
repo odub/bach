d6cb8530d249c5fa58f9d22321b84d813b6de78a
Oscar Dub <oscardub@gmail.com>
2018-09-06 17:46:11 -0400

    Add component scaffolding and base CSS for moment UI.

---
+++ client/src/Staff.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Staff.css';
+
+class Staff extends Component {
+  render() {
+    return <div className="Staff">Hello World!</div>;
+  }
+}
+
+export default Staff;
