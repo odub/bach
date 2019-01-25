c2add4fdbddcb1e2b737701a31ce00e00a8470b5
Oscar Dub <oscardub@gmail.com>
2019-01-25 16:44:30 +0000

    Improve start points.

--- client/scripts/generateStartPoints.js
+++ client/scripts/generateStartPoints.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const tonal = require('tonal-note');
+const distance = require('tonal-distance');
 
 const { musicXmlParse } = require('../../server/src/utils/pitch');
 
@@ -11,6 +11,18 @@ const noteNumbers = file
   .map(l => l.replace(/},\s*$/, '}').replace(/(^\s*\[|\]\s*$)/, ''))
   .map(JSON.parse)
   .map(o => o.notes.map(n => musicXmlParse(n)))
-  .filter(a => !a.some(v => v === null));
+  .filter(a => !a.some(v => v === null))
+  .sort((a, b) => {
+    let cmp = 0;
+    for (let i = a.length - 1; i >= 0; i--) {
+      cmp = distance.semitones('C4', a[i]) - distance.semitones('C4', b[i]);
+      if (cmp) {
+        return cmp;
+      }
+    }
+    return cmp;
+  })
+  .filter((v, i, a) => !a[i + 1] || a[i + 1].join(',') !== a[i].join(','))
+  .map(a => a.map(v => distance.interval('C4', v)));
 const output = JSON.stringify(noteNumbers, null, '  ');
 console.info(output);
